7e2ad329a1117b93d318c0ec905ff8f376bfb34d
Abhi <bhattiproluabhilash04@gmail.com>
2016-12-10 14:07:41 -0600

    Edited

---
+++ args.py
@@ -0,0 +1,9 @@
+from sys import argv
+script,first,second,third=argv
+
+print "The script is called:", script
+print "your first variabke is:",first
+print "Your second variable is:",second
+print "Your third variable is:",third
+best=raw_input("Give the name of your fav fruit")
+
